17ab2e80222eaafb3b23aa673d6ffc31a710b77b
Shree Saravanan B <shreesaravanan24@gmail.com>
2026-06-25 16:03:34 +0530

    Schedule

--- src/Schedule.java
+++ src/Schedule.java
@@ -6,7 +6,7 @@ public class Schedule {
             for (int week = 1; week <= 4; week++) {
 
                 for (int day = 1; day <= 7; day++) {
-
+                    if(week>=2){break;}
                     System.out.println("Month: " + month + " Week: " + week + " Day: " + day +" Days Completed: " + ++dayscompleted);
                     System.out.println("Come to College");
                 }
