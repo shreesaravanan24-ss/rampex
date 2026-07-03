b7fe84108a1faad2acadbef0375c138b7bd5abab
Shree Saravanan B <shreesaravanan24@gmail.com>
2026-07-03 15:22:11 +0530

    Anagram using nested loop, with forward and reverse search

---
+++ src/Day_12/Class_Task/Anagram.java
@@ -0,0 +1,28 @@
+package Day_12.Class_Task;
+
+public class Anagram {
+    public static void main(String [] args){
+        //isAnagram("pot", "top");
+        System.out.print(isAnagram("pot", "top"));
+    }
+    public static void /*boolean*/ isAnagram(String s1, String s2){
+        //input 2 strings arguments
+        //o/p: true or false
+        //Steps: 1. check and compare the length of both strings
+        //2. if length not same return false, else check further
+        if(s1.length() == s2.length()){
+            //further check // s2=top //s1=pot
+            for(int j=0; j<s2.length();j++) { //pot
+                for (int i = 0; i < s2.length(); i++) { //top
+                    if(s1.charAt(j)==s1.charAt(i)){
+
+                    }
+                    //System.out.println(s2.charAt(i));
+                }
+            }
+            //return true;
+        }else{
+            //return false;
+        }
+    }
+}
